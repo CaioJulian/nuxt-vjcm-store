4a172885f60e37c003de465573c4fca8fcfb13db
Caio <caiojulian@hotmail.com>
2021-11-09 22:16:46 -0300

    stub data with mirage

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,14 +1,46 @@
 import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard.vue';
+import { makeServer } from '@/miragejs/server';
 
 describe('ProductCard - unit', () => {
-  it('should mount the component', () => {
+  let server;
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it('should match snapshot', () => {
     const wrapper = mount(ProductCard, {
       propsData: {
-        product: {},
+        product: server.create('product', {
+          title: 'Watch Rolex',
+          price: '12.00',
+          image:
+            'https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        }),
       },
     });
 
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should mount the component', () => {
+    const wrapper = mount(ProductCard, {
+      propsData: {
+        product: server.create('product', {
+          title: 'Watch Rolex',
+          price: '12.00',
+        }),
+      },
+    });
+
+    // console.log(wrapper.html());
+
     expect(wrapper.vm).toBeDefined();
+    expect(wrapper.text()).toContain('Watch Rolex');
+    expect(wrapper.text()).toContain('$12.00');
   });
 });
