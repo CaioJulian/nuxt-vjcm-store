cebb651e8163064df1ab556794b5578c0b2b8e71
Caio <caiojulian@hotmail.com>
2022-02-13 23:32:27 -0300

    config shortcut commands cypress

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -4,6 +4,8 @@ import { makeServer } from '../../miragejs/server';
 
 context('Store', () => {
   let server;
+  const cg = cy.get;
+  const cgId = cy.getByTestId;
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
@@ -16,51 +18,51 @@ context('Store', () => {
   it('should display the store', () => {
     cy.visit('/');
 
-    cy.get('body').contains('Brand');
-    cy.get('body').contains('Wrist Watch');
+    cg('body').contains('Brand');
+    cg('body').contains('Wrist Watch');
   });
 
   context('Store > Shopping Cart', () => {
     it('should not display shopping cart when page first loads', () => {
       cy.visit('/');
 
-      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+      cgId('shopping-cart').should('have.class', 'hidden');
     });
 
     it('should toggle shopping cart visibility when button is clicked', () => {
       cy.visit('/');
-      cy.get('[data-testid="toggle-button"]').as('toggleButton');
-      cy.get('@toggleButton').click();
-      cy.get('[data-testid="shopping-cart"]').should(
+      cgId('toggle-button').as('toggleButton');
+      cg('@toggleButton').click();
+      cgId('shopping-cart').should(
         'not.have.class',
         'hidden'
       );
-      cy.get('@toggleButton').click({ force: true });
-      cy.get('[data-testid="shopping-cart"]').should('have.class', 'hidden');
+      cg('@toggleButton').click({ force: true });
+      cgId('shopping-cart').should('have.class', 'hidden');
     });
   });
 
   context('Store > Product List', () => {
     it('should display "0 Products" when no products is returned', () => {
       cy.visit('/');
-      cy.get('[data-testid="product-card"]').should('have.length', 0);
-      cy.get('body').contains('0 Products');
+      cgId('product-card').should('have.length', 0);
+      cg('body').contains('0 Products');
     });
 
     it('should display "1 Product" when 1 product is returned', () => {
       server.create('product');
 
       cy.visit('/');
-      cy.get('[data-testid="product-card"]').should('have.length', 1);
-      cy.get('body').contains('1 Product');
+      cgId('product-card').should('have.length', 1);
+      cg('body').contains('1 Product');
     });
 
     it('should display "10 Products" when 10 products are returned', () => {
       server.createList('product', 10);
 
       cy.visit('/');
-      cy.get('[data-testid="product-card"]').should('have.length', 10);
-      cy.get('body').contains('10 Products');
+      cgId('product-card').should('have.length', 10);
+      cg('body').contains('10 Products');
     });
   });
 
@@ -68,7 +70,7 @@ context('Store', () => {
     it('should type in the search field', () => {
       cy.visit('/');
 
-      cy.get('input[type="search"]')
+      cg('input[type="search')
         .type('Some text here')
         .should('have.value', 'Some text here');
     });
@@ -80,20 +82,20 @@ context('Store', () => {
       server.createList('product', 10);
 
       cy.visit('/');
-      cy.get('input[type="search"]').type('Watch Mi');
-      cy.get('[data-testid="search-form"]').submit();
-      cy.get('[data-testid="product-card"]').should('have.length', 1);
-      cy.get('body').contains('1 Product');
+      cg('input[type="search').type('Watch Mi');
+      cgId('search-form').submit();
+      cgId('product-card').should('have.length', 1);
+      cg('body').contains('1 Product');
     });
 
     it('should not return any product', () => {
       server.createList('product', 10);
 
       cy.visit('/');
-      cy.get('input[type="search"]').type('Watch Mi');
-      cy.get('[data-testid="search-form"]').submit();
-      cy.get('[data-testid="product-card"]').should('have.length', 0);
-      cy.get('body').contains('0 Products');
+      cg('input[type="search').type('Watch Mi');
+      cgId('search-form').submit();
+      cgId('product-card').should('have.length', 0);
+      cg('body').contains('0 Products');
     });
   });
 });
