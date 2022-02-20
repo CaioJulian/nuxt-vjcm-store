90337cfe50f2032fb58841529b580be64cbe4a84
Caio Julian <caiojulian@hotmail.com>
2022-02-20 16:33:49 -0300

    config github actions - husky - netlify (#8)
    - config github actions: pull-request, merge-develop, merge-main;
    - config husky: pre-commit, pre-push;
    - config deploy netlify.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,26 +1,21 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nvjc-store',
+    title: 'nuxt-vjcm-store',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/miragejs'
-  ],
+  plugins: ['@/plugins/miragejs'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: false,
@@ -47,11 +42,10 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
